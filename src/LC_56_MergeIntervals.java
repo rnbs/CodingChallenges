a28b0f2dca8be967514c0fafe81297ce4dab31cb
rnbsantosh <89754606+rnbsantosh@users.noreply.github.com>
2024-05-14 19:57:56 -0700

    few more coding challenges

--- src/LC_56_MergeIntervals.java
+++ src/LC_56_MergeIntervals.java
@@ -1,29 +1,33 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class LC_56_MergeIntervals {
     public static int[][] mergeIntervals(int[][] intervals) {
-        int[][] result = new int[][]{};
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
 
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-
-        for(int[] interval : intervals) {
-            for(int i =1; i < interval.length; i++) {
-                int currStart = interval[0];
-                int currEnd = interval[1];
-
-                if(currStart <= end) {
-                    end = Math.max(currEnd, end);
-                } else {
-                    result.add(new int[]{currStart, currEnd});
-                }
-
+        List<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || ((LinkedList<int[]>) merged).getLast()[1] < interval[0]) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                ((LinkedList<int[]>) merged).getLast()[1] = Math.max(
+                        ((LinkedList<int[]>) merged).getLast()[1],
+                        interval[1]
+                );
             }
         }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] result = mergeIntervals(intervals);
+        Arrays.stream(result).forEach(x -> System.out.println(x[0] + " " + x[1]));
     }
 
 }
